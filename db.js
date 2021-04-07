ed107f918eb1d8fe9f253e73ea15d76bc6f0eb08
Ananthu <ananthugiridhar@gmail.com>
2021-04-07 14:00:27 +0530

    final changes 02

--- db.js
+++ db.js
@@ -1,10 +1,15 @@
 const Pool = require("pg").Pool;
-//require('dotenv').config();
+require('dotenv').config();
 
+const userid = (process.env.DB_USER);
+const password = (process.env.DB_PASSWORD);
+const database = (process.env.DB_DATABASE);
+const host = (process.env.DB_HOST);
+const port = process.env.DB_PORT;
 
 
 const pool = new Pool({
-    user: 'sagvlgma',
+    user: userid,
     password: 'zsnnSdlBcDPNxUTLyWvTAiJIXXiiQhwW',
     database: 'sagvlgma',
     host: 'queenie.db.elephantsql.com',
